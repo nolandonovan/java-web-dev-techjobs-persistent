e181d57709e6541cd8f41c73732b2694c77efe2f
nolandonovan <nolandonovan24@gmail.com>
2020-03-14 20:12:11 -0500

    Models complete for part 2

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,28 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    public Employer(String location) {
+        super();
+        this.location = location;
+    }
 
+    public Employer() {}
+
+    @NotBlank
+    @Size(max = 100, message = "Location must be less than 100 characters long.")
+    private String location;
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,26 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @Size(max = 1500, message = "Skill's long description cannot be longer than 1,500 characters")
+    private String longDescription;
+
+    public Skill(String longDescription) {
+        super();
+        this.longDescription = longDescription;
+    }
+
+    public Skill() {}
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+
+    public void setLongDescription(String longDescription) {
+        this.longDescription = longDescription;
+    }
 }
